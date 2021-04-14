3476abb96a4b7c49cc3780aaf116df9d16015619
NguyenThiHaNi <56101259+NguyenThiHaNi@users.noreply.github.com>
2021-04-14 13:42:00 +0700

    Delete index6.js

--- index6.js
+++
@@ -1,9 +0,0 @@
-
-function random(min,max){
-    return Math.floor(Math.random() * (max - min)) + min;
-}   
-console.log(random());
-function elementarr(array){
-  return array[random(0,array.length-1)];
-}
-console.log(elementarr());
